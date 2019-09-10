1b438feaa66675ecfa90af75f5677eb97367f31f
Dexter Leng <dexterlengcs@gmail.com>
2019-09-10 15:54:39 +0800

    Added RxSwift dependency

--- Pods/Target Support Files/Pods-ViMac-Swift/Pods-ViMac-Swift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ViMac_Swift : NSObject
-@end
-@implementation PodsDummy_Pods_ViMac_Swift
-@end
--- Pods/Target Support Files/Pods-ViMac-SwiftUITests/Pods-ViMac-SwiftUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ViMac_SwiftUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ViMac_SwiftUITests
-@end
