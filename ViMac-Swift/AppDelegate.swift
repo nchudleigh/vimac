bedbb436c730d18ee0f43535456fc307a3ad1e3b
Dexter Leng <dexterlengcs@gmail.com>
2019-09-18 16:38:15 +0800

    Move static functions that create observables to another file

--- ViMac-Swift/AppDelegate.swift
+++ ViMac-Swift/AppDelegate.swift
@@ -14,19 +14,13 @@ import os
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    // This struct allows us to propagate the original source value (application) when doing a flatMap/flatMapLatest to get the notification.
-    struct AppNotificationAppPair {
-        let app: Application?
-        let notification: AXNotification?
-    }
-
     let borderWindowController: NSWindowController
     let storyboard: NSStoryboard
     let hintShortcut: MASShortcut
     let scrollShortcut: MASShortcut
     
     let applicationObservable: Observable<Application?>
-    let applicationNotificationObservable: Observable<AppNotificationAppPair>
+    let applicationNotificationObservable: Observable<AccessibilityObservables.AppNotificationAppPair>
     let windowSubject: BehaviorSubject<UIElement?>
     let overlayEventSubject: PublishSubject<OverlayEvent>
     
@@ -38,74 +32,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     let HINT_TEXT_FIELD_TAG = 1
     let SCROLL_TEXT_FIELD_TAG = 2
     let SCROLL_SELECTOR_TEXT_FIELD_TAG = 3
-
-    static func createApplicationObservable() -> Observable<Application?> {
-        return Observable.create { observer in
-            let center = NSWorkspace.shared.notificationCenter
-            center.addObserver(forName: NSWorkspace.didDeactivateApplicationNotification, object: nil, queue: nil) { notification in
-                if let nsApplication = NSWorkspace.shared.frontmostApplication,
-                    let application = Application.init(nsApplication) {
-                    os_log("Current frontmost application: %@", log: Log.accessibility, String(describing: application))
-                    observer.on(.next(application))
-                } else {
-                    os_log("Current frontmost application: nil", log: Log.accessibility)
-                    observer.on(.next(nil))
-                }
-            }
-            let cancel = Disposables.create {
-                center.removeObserver(self)
-                os_log("Removed application observer", log: Log.accessibility)
-            }
-            
-            return cancel
-        }
-    }
-    
-    static func createApplicationNotificationObservable(applicationObservable: Observable<Application?>) -> Observable<AppNotificationAppPair> {
-        return applicationObservable
-            .flatMapLatest { appOptional -> Observable<AppNotificationAppPair> in
-                if let app = appOptional {
-                    return Observable.create { observer in
-                        let notificationObserver = app.createObserver { (_observer: Observer, _element: UIElement, event: AXNotification) in
-                            os_log("New app notification: %@", log: Log.accessibility, String(describing: event))
-                            let pair = AppNotificationAppPair(app: app, notification: event)
-                            observer.on(.next(pair))
-                        }
-                        
-                        let events = [AXNotification.focusedWindowChanged] + windowEvents
-                        for event in events {
-                            do {
-                                try notificationObserver?.addNotification(event, forElement: app)
-                            } catch {
-                                os_log("Error adding notification observer for event: %@ and application %@. Error: %@", log: Log.accessibility, type: .error, String(describing: event), String(describing: app), String(describing: error))
-                            }
-                        }
-                        
-                        let cancel = Disposables.create {
-                            
-                            for event in events {
-                                do {
-                                    try notificationObserver?.removeNotification(event, forElement: app)
-                                } catch {
-                                    os_log("Error removing notification observer for event: %@ and application %@. Error: %@", log: Log.accessibility, type: .error, String(describing: event), String(describing: app), String(describing: error))
-                                }
-                            }
-                            os_log("Removed notification observers for %@.", log: Log.accessibility, String(describing: app))
-                        }
-                        return cancel
-                    }
-                } else {
-                    return Observable.just(AppNotificationAppPair(app: nil, notification: nil))
-                }
-        }
-    }
     
     override init() {
         storyboard =
             NSStoryboard.init(name: "Main", bundle: nil)
         borderWindowController = storyboard.instantiateController(withIdentifier: "overlayWindowControllerID") as! NSWindowController
-        applicationObservable = AppDelegate.createApplicationObservable().share()
-        applicationNotificationObservable = AppDelegate.createApplicationNotificationObservable(applicationObservable: applicationObservable)
+        applicationObservable = AccessibilityObservables.createApplicationObservable().share()
+        applicationNotificationObservable = AccessibilityObservables.createApplicationNotificationObservable(applicationObservable: applicationObservable, notifications: AppDelegate.windowEvents + [AXNotification.focusedWindowChanged])
         windowSubject = BehaviorSubject(value: nil)
         overlayEventSubject = PublishSubject()
         hintShortcut = MASShortcut.init(keyCode: kVK_Space, modifierFlags: [.command, .option, .control])
