bebb47e86b9f699d99868c53bbd6dbcd64cb6690
Dexter Leng <dexterlengcs@gmail.com>
2019-09-19 16:39:00 +0800

    Add icon to menu bar

---
+++ ViMac-Swift/Windows/Preferences/PreferencesWindowController.swift
@@ -0,0 +1,13 @@
+//
+//  PreferencesWindowController.swift
+//  ViMac-Swift
+//
+//  Created by Huawei Matebook X Pro on 19/9/19.
+//  Copyright © 2019 Dexter Leng. All rights reserved.
+//
+
+import Cocoa
+
+class PreferencesWindowController: NSWindowController, NSWindowDelegate {
+    @IBOutlet weak var preferencesWindow: NSWindow!
+}
