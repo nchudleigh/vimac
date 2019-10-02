8799cc0457e8219346386167256219417e623e94
Dexter Leng <dexterlengcs@gmail.com>
2019-10-02 22:57:13 +0800

    Blacklist AXWindow

--- ViMac-Swift/Utils.swift
+++ ViMac-Swift/Utils.swift
@@ -138,7 +138,7 @@ class Utils: NSObject {
 
             if let position = positionOptional,
                 let role = roleOptional {
-                let blacklistedRoles = ["AXUnknown", "AXStaticText", "AXToolbar", "AXCell"]
+                let blacklistedRoles = ["AXUnknown", "AXStaticText", "AXToolbar", "AXCell", "AXWindow"]
                 let isGroupRole = role.hasSuffix("Group")
                 let isBlacklisted = blacklistedRoles.contains(role)
                 if (windowFrame.contains(position) && !isGroupRole && !isBlacklisted) {
