40589265a6242eb05eef232a1d8db104461a2c89
Dexter Leng <dexterlengcs@gmail.com>
2019-10-06 19:01:18 +0800

    Make deploy script generate appcast

--- deploy_setup.sh
+++ deploy_setup.sh
@@ -1,2 +1,7 @@
 xcodebuild archive -scheme Vimac -workspace Vimac.xcworkspace/ -archivePath ./build/production.xcarchive
 xcodebuild -exportArchive -archivePath ./build/production.xcarchive/ -exportPath ../VimacBuilds -exportOptionsPlist ExportOptions.plist
+VERSION=`/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" ./ViMac-Swift/Info.plist`
+FILENAME="vimac-${VERSION}.app.zip"
+zip -r ../VimacBuilds/"${FILENAME}" ../VimacBuilds/Vimac.app/
+./Pods/Sparkle/bin/generate_appcast ../VimacBuilds/
+rm -rf ../VimacBuilds/Vimac.app
