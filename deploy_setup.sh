c013616f64ad88dcaca15b37a71b20809b3cfa0b
Dexter Leng <dexterlengcs@gmail.com>
2019-11-26 22:51:40 +0800

    Delete unused deploy script

--- deploy_setup.sh
+++
@@ -1,7 +0,0 @@
-xcodebuild archive -scheme Vimac -workspace Vimac.xcworkspace/ -archivePath ./build/production.xcarchive
-xcodebuild -exportArchive -archivePath ./build/production.xcarchive/ -exportPath ../VimacBuilds -exportOptionsPlist ExportOptions.plist
-VERSION=`/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" ./ViMac-Swift/Info.plist`
-FILENAME="vimac-${VERSION}.app.zip"
-zip -r ../VimacBuilds/"${FILENAME}" ../VimacBuilds/Vimac.app/
-./Pods/Sparkle/bin/generate_appcast ../VimacBuilds/
-rm -rf ../VimacBuilds/Vimac.app
