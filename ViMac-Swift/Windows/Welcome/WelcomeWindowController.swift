9c57470919c3b1c18a9d6ae0413bd78263f719ad
Dexter Leng <dexterlengcs@gmail.com>
2020-01-11 16:28:37 +0800

    Show welcome window when accessibility permissions not granted on launch

---
+++ ViMac-Swift/Windows/Welcome/WelcomeWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  WelcomeWindowController.swift
+//  Vimac
+//
+//  Created by Dexter Leng on 11/1/20.
+//  Copyright © 2020 Dexter Leng. All rights reserved.
+//
+
+import Cocoa
+
+class WelcomeWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
