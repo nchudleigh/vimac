1bea1fb8f200738196dc243a4a8b48c615ca356a
Dexter Leng <dexterlengcs@gmail.com>
2020-06-14 16:19:26 +0800

    InputListener - short circuit events to prevent doot doot sounds

--- ViMac-Swift/InputListener.swift
+++ ViMac-Swift/InputListener.swift
@@ -4,9 +4,11 @@ import RxSwift
 class InputListener {
     let events: Observable<NSEvent> =
         Observable.create({ observer in
-            let keyMonitor = NSEvent.addLocalMonitorForEvents(matching: NSEvent.EventTypeMask.keyDown.union(.keyUp), handler: { event -> NSEvent in
+            let keyMonitor = NSEvent.addLocalMonitorForEvents(matching: NSEvent.EventTypeMask.keyDown.union(.keyUp), handler: { event -> NSEvent? in
                 observer.onNext(event)
-                return event
+                // return nil to prevent the event from being dispatched
+                // this removes the "doot doot" sound when typing with CMD / CTRL held down
+                return nil
             })!
             
             let cancel = Disposables.create {
