eef1fb1c19838f83dae3e8509a1e4ec1730b7543
Dexter Leng <dexterlengcs@gmail.com>
2021-01-26 00:00:30 +0800

    Do not force unwrap creating event tap

--- ViMac-Swift/GlobalEventTap.swift
+++ ViMac-Swift/GlobalEventTap.swift
@@ -28,10 +28,10 @@ class GlobalEventTap {
         return CGEvent.tapIsEnabled(tap: tap)
     }
     
-    func enable() {
+    func enable() -> Bool {
         if let tap = eventTap {
             if CGEvent.tapIsEnabled(tap: tap) {
-                return
+                return true
             } else {
                 CFMachPortInvalidate(tap)
                 CFRunLoopRemoveSource(CFRunLoopGetCurrent(), runLoopSource, .commonModes)
@@ -40,10 +40,13 @@ class GlobalEventTap {
             }
         }
 
-        self.eventTap = createEventTap()
+        guard let tap = createEventTap() else { return false }
+        self.eventTap = tap
         self.runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0)
         CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, .commonModes)
         CGEvent.tapEnable(tap: self.eventTap!, enable: true)
+        
+        return true
     }
     
     func disable() {
@@ -72,13 +75,13 @@ class GlobalEventTap {
         return nil
     }
     
-    private func createEventTap() -> CFMachPort {
+    private func createEventTap() -> CFMachPort? {
         let eventTap = CGEvent.tapCreate(tap: .cgSessionEventTap, place: .headInsertEventTap, options: .defaultTap, eventsOfInterest: eventMask, callback: { proxy, type, event, refcon in
             // Trick from https://stackoverflow.com/questions/33260808/how-to-use-instance-method-as-callback-for-function-which-takes-only-func-or-lit
             let mySelf = Unmanaged<GlobalEventTap>.fromOpaque(refcon!).takeUnretainedValue()
             return mySelf.eventTapCallback(proxy: proxy, type: type, event: event, refcon: refcon)
         },
-        userInfo: selfPtr.toOpaque())!
+        userInfo: selfPtr.toOpaque())
         return eventTap
     }
 }
