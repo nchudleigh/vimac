263ba51f3ea79696254912dee6ac3182cac2e204
Dexter Leng <34204380+dexterleng@users.noreply.github.com>
2021-02-24 21:10:16 +0800

    Hint mode refactor (#314)
    * Create Hint for representing a hint
    
    * HintModeViewController - log as a side effect, exit mode on query error
    
    * oops
    
    * Remove force-cast
    
    * Move rendering of hints to HintsViewController
    
    * clean up hint clicking and revert mouse location code
    
    * Move hint querying to HintModeQueryService

--- ViMac-Swift/ViewControllers/HintModeViewController.swift
+++ ViMac-Swift/ViewControllers/HintModeViewController.swift
@@ -12,18 +12,36 @@ import RxSwift
 import Carbon.HIToolbox
 import os
 
+struct Hint {
+    let element: Element
+    let text: String
+}
+
+enum HintAction {
+    case leftClick
+    case rightClick
+    case doubleLeftClick
+}
+
 class HintModeViewController: ModeViewController, NSTextFieldDelegate {
     let app: NSRunningApplication
     let window: Element
-    lazy var elements: Single<[Element]> = elementObservable().toArray()
+    
+    var hints: [Hint]?
+    var hintsViewController: HintsViewController?
+    
     lazy var inputListeningTextField = instantiateInputListeningTextField()
-    var hintViews: [HintView]?
-    let compositeDisposable = CompositeDisposable()
     let inputListener = HintModeInputListener()
+    
     var characterStack: [Character] = [Character]()
-    let originalMousePosition = NSEvent.mouseLocation
     let startTime = CFAbsoluteTimeGetCurrent()
-
+    
+    // preferences
+    let possibleHintCharacters = UserPreferences.HintMode.CustomCharactersProperty.read()
+    let textSize = UserPreferences.HintMode.TextSizeProperty.readAsFloat()
+    
+    let disposeBag = DisposeBag()
+    
     init(app: NSRunningApplication, window: Element) {
         self.app = app
         self.window = window
@@ -44,145 +62,99 @@ class HintModeViewController: ModeViewController, NSTextFieldDelegate {
         observeDeleteKey()
         observeSpaceKey()
         
-        _ = self.compositeDisposable.insert(observeElements())
-        
         hideMouse()
+
+        HintModeQueryService.init(app: app, window: window, hintCharacters: possibleHintCharacters).perform()
+            .toArray()
+            .observeOn(MainScheduler.instance)
+            .do(onSuccess: { _ in self.logQueryTime() })
+            .do(onError: { e in self.logError(e) })
+            .subscribe(
+                onSuccess: { self.onHintQueryCompleted(hints: $0) },
+                onError: { _ in self.modeCoordinator?.exitMode()}
+            )
+            .disposed(by: disposeBag)
     }
     
-    func elementObservable() -> Observable<Element> {
-        return Utils.eagerConcat(observables: [
-            Utils.singleToObservable(single: queryWindowElementsSingle()),
-            Utils.singleToObservable(single: queryMenuBarSingle()),
-            Utils.singleToObservable(single: queryMenuBarExtrasSingle()),
-            Utils.singleToObservable(single: queryNotificationCenterSingle())
-        ])
+    func logQueryTime() {
+        let timeElapsed = CFAbsoluteTimeGetCurrent() - self.startTime
+        os_log("[Hint mode] query time: %@", log: Log.accessibility, String(describing: timeElapsed))
     }
     
-    func queryWindowElementsSingle() -> Single<[Element]> {
-        return Single.create(subscribe: { [weak self] event in
-            guard let self = self else {
-                event(.success([]))
-                return Disposables.create()
-            }
-            
-            let thread = Thread.init(block: {
-                let service = QueryWindowService.init(app: self.app, window: self.window)
-                let elements = try? service.perform()
-                event(.success(elements ?? []))
-            })
-            thread.start()
-            return Disposables.create {
-                thread.cancel()
-            }
-        })
+    func logError(_ e: Error) {
+        os_log("[Hint mode] query error: %@", log: Log.accessibility, String(describing: e))
     }
-    
-    func queryMenuBarSingle() -> Single<[Element]> {
-        return Single.create(subscribe: { [weak self] event in
-            guard let self = self else {
-                event(.success([]))
-                return Disposables.create()
-            }
-            
-            let thread = Thread.init(block: {
-                // as of 28e46b9cbe9a38e7c43c1eb1f0d8953d99bc5ef9,
-                // when one activates hint mode when the Vimac preference page is frontmost,
-                // the app crashes with EXC_BAD_INSTRUCTION when retrieving menu bar items attributes through Element.initialize
-                // I suspect that threading is the cause of crashing when reading attributes from your own app
-                let isVimac = self.app.bundleIdentifier == Bundle.main.bundleIdentifier
-                if isVimac {
-                    event(.success([]))
-                    return
-                }
-                
-                let service = QueryMenuBarItemsService.init(app: self.app)
-                let elements = try? service.perform()
-                event(.success(elements ?? []))
-            })
-            thread.start()
-            return Disposables.create {
-                thread.cancel()
-            }
-        })
-    }
-    
-    func queryMenuBarExtrasSingle() -> Single<[Element]> {
-        return Single.create(subscribe: { event in
-            let thread = Thread.init(block: {
-                let service = QueryMenuBarExtrasService.init()
-                let elements = try? service.perform()
-                event(.success(elements ?? []))
-            })
-            thread.start()
-            return Disposables.create {
-                thread.cancel()
-            }
-        })
-    }
-    
-    func queryNotificationCenterSingle() -> Single<[Element]> {
-        return Single.create(subscribe: { event in
-            let thread = Thread.init(block: {
-                let service = QueryNotificationCenterItemsService.init()
-                let elements = try? service.perform()
-                event(.success(elements ?? []))
-            })
-            thread.start()
-            return Disposables.create {
-                thread.cancel()
-            }
-        })
-    }
-    
+
     func onLetterKeyDown(event: NSEvent) {
         guard let character = event.charactersIgnoringModifiers?.first else { return }
-        guard let hintViews = self.hintViews else { return }
-
+        guard let hints = hints else { return }
+        
         self.characterStack.append(character)
         let typed = String(self.characterStack)
 
-        let matchingHints = hintViews.filter { hintView in
-            return hintView.hintTextView!.stringValue.starts(with: typed.uppercased())
-        }
-
+        let matchingHints = hints.filter { $0.text.starts(with: typed.uppercased()) }
+    
         if matchingHints.count == 0 && typed.count > 0 {
             self.modeCoordinator?.exitMode()
             return
         }
 
         if matchingHints.count == 1 {
-            let matchingHint = matchingHints.first!
-            let element = matchingHint.associatedElement
-
-            let frame = element.clippedFrame ?? element.frame
-            let position = frame.origin
-            let size = frame.size
-
-            let centerPositionX = position.x + (size.width / 2)
-            let centerPositionY = position.y + (size.height / 2)
-            let centerPosition = NSPoint(x: centerPositionX, y: centerPositionY)
-
+            let hint = matchingHints.first!
+            
             // close the window before performing click(s)
             // Chrome's bookmark bar doesn't let you right click if Chrome is not the active window
             self.modeCoordinator?.exitMode()
+
+            let originalMousePosition: NSPoint = {
+                let invertedPos = NSEvent.mouseLocation
+                let frame = GeometryUtils.convertAXFrameToGlobal(
+                    NSRect(
+                        origin: invertedPos,
+                        size: NSSize.zero
+                    )
+                )
+                return frame.origin
+            }()
+            let action: HintAction = {
+                if (event.modifierFlags.rawValue & NSEvent.ModifierFlags.shift.rawValue == NSEvent.ModifierFlags.shift.rawValue) {
+                    return .rightClick
+                } else if (event.modifierFlags.rawValue & NSEvent.ModifierFlags.command.rawValue == NSEvent.ModifierFlags.command.rawValue) {
+                    return .doubleLeftClick
+                } else {
+                    return .leftClick
+                }
+            }()
+            performHintAction(hint, action: action)
             
-            Utils.moveMouse(position: centerPosition)
-            
-            if (event.modifierFlags.rawValue & NSEvent.ModifierFlags.shift.rawValue == NSEvent.ModifierFlags.shift.rawValue) {
-                Utils.rightClickMouse(position: centerPosition)
-            } else if (event.modifierFlags.rawValue & NSEvent.ModifierFlags.command.rawValue == NSEvent.ModifierFlags.command.rawValue) {
-                Utils.doubleLeftClickMouse(position: centerPosition)
-            } else if (event.modifierFlags.rawValue & NSEvent.ModifierFlags.control.rawValue == NSEvent.ModifierFlags.control.rawValue) {
-            } else {
-                Utils.leftClickMouse(position: centerPosition)
-            }
-            
-            revertMouseLocation()
-            return
+            Utils.moveMouse(position: originalMousePosition)
         }
 
         // update hints to reflect new typed text
         self.updateHints(typed: typed)
+    }
+    
+    func performHintAction(_ hint: Hint, action: HintAction) {
+        let element = hint.element
+
+        let frame = element.clippedFrame ?? element.frame
+        let position = frame.origin
+        let size = frame.size
+
+        let centerPositionX = position.x + (size.width / 2)
+        let centerPositionY = position.y + (size.height / 2)
+        let centerPosition = NSPoint(x: centerPositionX, y: centerPositionY)
+
+        Utils.moveMouse(position: centerPosition)
+        
+        switch action {
+        case .leftClick:
+            Utils.leftClickMouse(position: centerPosition)
+        case .rightClick:
+            Utils.rightClickMouse(position: centerPosition)
+        case .doubleLeftClick:
+            Utils.doubleLeftClickMouse(position: centerPosition)
+        }
     }
     
     func observeLetterKeyDown() {
@@ -213,107 +185,34 @@ class HintModeViewController: ModeViewController, NSTextFieldDelegate {
         })
     }
     
-    func observeElements() -> Disposable {
-        return elements
-            .observeOn(MainScheduler.instance)
-            .subscribe(
-                onSuccess: { [weak self] elements in
-                    let timeElapsed = CFAbsoluteTimeGetCurrent() - self!.startTime
-                    os_log("[Hint mode] query time: %@", log: Log.accessibility, String(describing: timeElapsed))
-                    
-                    self?.onElementTraversalComplete(elements: elements)
-                },
-                onError: { error in
-                    print(error)
-                }
-            )
-    }
-    
-    func onElementTraversalComplete(elements: [Element]) {
-        let hintStrings = AlphabetHints().hintStrings(linkCount: elements.count, hintCharacters: UserPreferences.HintMode.CustomCharactersProperty.read())
-        
-        let textSize = UserPreferences.HintMode.TextSizeProperty.readAsFloat()
+    func onHintQueryCompleted(hints: [Hint]) {
+        self.hints = hints
 
-        let hintViews: [HintView] = elements
-            .enumerated()
-            .map ({ (index, element) in
-                return instantiateHintView(associatedElement: element, textSize: CGFloat(textSize), text: hintStrings[index])
-            })
-            .compactMap({ $0 })
-        
-        self.hintViews = hintViews
+        self.hintsViewController = HintsViewController(hints: self.hints!, textSize: CGFloat(textSize), hintCharacters: possibleHintCharacters)
+        self.addChild(hintsViewController!)
+        hintsViewController!.view.frame = self.view.frame
+        self.view.addSubview(hintsViewController!.view)
 
-        for hintView in hintViews {
-            self.view.addSubview(hintView)
-        }
-        
         self.inputListeningTextField.becomeFirstResponder()
     }
-    
-    func instantiateHintView(associatedElement: Element, textSize: CGFloat, text: String) -> HintView? {
-        let text = HintView(associatedElement: associatedElement, hintTextSize: CGFloat(textSize), hintText: text, typedHintText: "")
-        
-        let centerPositionOptional: NSPoint? = {
-            do {
-                let globalElementFrame = GeometryUtils.convertAXFrameToGlobal(associatedElement.frame)
-                let screenOrigin = self.view.window!.frame.origin
-                let elementFrameRelativeToScreen = GeometryUtils.convertGlobalFrame(globalElementFrame, relativeTo: screenOrigin)
-                let elementCenter: NSPoint = GeometryUtils.center(elementFrameRelativeToScreen)
-                
-                let hintOrigin = NSPoint(
-                    x: elementCenter.x - (text.frame.size.width / 2),
-                    y: elementCenter.y - (text.frame.size.height / 2)
-                )
 
-                if hintOrigin.x.isNaN || hintOrigin.y.isNaN {
-                    return nil
-                }
-                
-                return hintOrigin
-            } 
-        }()
-
-        guard let centerPosition = centerPositionOptional else {
-            return nil
+    private func removeChildViewController(_ vc: NSViewController) {
+        if !self.children.contains(vc) {
+            return
         }
-        text.frame.origin = centerPosition
         
-        return text
+        vc.view.removeFromSuperview()
+        vc.removeFromParent()
     }
     
     func updateHints(typed: String) {
-        guard let hintViews = self.hintViews else {
-            self.modeCoordinator?.exitMode()
-            return
-        }
-
-        hintViews.forEach { hintView in
-            hintView.isHidden = true
-            if hintView.hintTextView!.stringValue.starts(with: typed.uppercased()) {
-                hintView.updateTypedText(typed: typed)
-                hintView.isHidden = false
-            }
-        }
+        guard let hintsVC = hintsViewController else { return }
+        hintsVC.updateTyped(typed: typed)
     }
     
-    // randomly rotate hints
-    // ideally we group them into clusters of intersecting hints and rotate within those clusters
-    // but this is just a quick fast hack
     func rotateHints() {
-        guard let hintViews = self.hintViews else {
-            self.modeCoordinator?.exitMode()
-            return
-        }
-        
-        for hintView in hintViews {
-            hintView.removeFromSuperview()
-        }
-        
-        let shuffledHintViews = hintViews.shuffled()
-        for hintView in shuffledHintViews {
-            self.view.addSubview(hintView)
-        }
-        self.hintViews = shuffledHintViews
+        guard let hintsVC = hintsViewController else { return }
+        hintsVC.rotateHints()
     }
     
     func hideMouse() {
@@ -323,20 +222,9 @@ class HintModeViewController: ModeViewController, NSTextFieldDelegate {
     func showMouse() {
         HideCursorGlobally.unhide()
     }
-    
-    private func revertMouseLocation() {
-        let frame = GeometryUtils.convertAXFrameToGlobal(
-            NSRect(
-                origin: originalMousePosition,
-                size: NSSize.zero
-            )
-        )
-        Utils.moveMouse(position: frame.origin)
-    }
 
     override func viewDidDisappear() {
         super.viewDidDisappear()
-        self.compositeDisposable.dispose()
         
         showMouse()
     }
---
+++ ViMac-Swift/ViewControllers/HintsViewController.swift
@@ -0,0 +1,96 @@
+//
+//  HintsViewController.swift
+//  Vimac
+//
+//  Created by Dexter Leng on 24/2/21.
+//  Copyright © 2021 Dexter Leng. All rights reserved.
+//
+
+import Cocoa
+
+class HintsViewController: NSViewController {
+    let hints: [Hint]
+    let textSize: CGFloat
+    var typed: String
+
+    var hintViews: [HintView]!
+    
+    init(hints: [Hint], textSize: CGFloat, hintCharacters: String, typed: String = "") {
+        self.hints = hints
+        self.textSize = textSize
+        self.typed = typed
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func loadView() {
+        self.view = NSView()
+    }
+
+    override func viewDidAppear() {
+        super.viewDidAppear()
+        
+        self.hintViews = hints
+            .map { renderHint($0) }
+            .compactMap({ $0 })
+
+        for hintView in self.hintViews {
+            self.view.addSubview(hintView)
+        }
+    }
+    
+    func updateTyped(typed: String) {
+        guard let hintViews = self.hintViews else { return }
+        
+        self.typed = typed
+        hintViews.forEach { hintView in
+            hintView.isHidden = true
+            if hintView.hintTextView!.stringValue.starts(with: typed.uppercased()) {
+                hintView.updateTypedText(typed: typed)
+                hintView.isHidden = false
+            }
+        }
+    }
+    
+    func rotateHints() {
+        for hintView in hintViews {
+            hintView.removeFromSuperview()
+        }
+        
+        let shuffledHintViews = hintViews.shuffled()
+        for hintView in shuffledHintViews {
+            self.view.addSubview(hintView)
+        }
+        self.hintViews = shuffledHintViews
+    }
+    
+    func renderHint(_ hint: Hint) -> HintView? {
+        let view = HintView(associatedElement: hint.element, hintTextSize: CGFloat(textSize), hintText: hint.text, typedHintText: "")
+        guard let elementFrame = self.elementFrame(hint.element) else { return nil }
+        let elementCenter: NSPoint = GeometryUtils.center(elementFrame)
+        
+        let hintOrigin = NSPoint(
+            x: elementCenter.x - (view.frame.size.width / 2),
+            y: elementCenter.y - (view.frame.size.height / 2)
+        )
+
+        if hintOrigin.x.isNaN || hintOrigin.y.isNaN {
+            return nil
+        }
+
+        view.frame.origin = hintOrigin
+        return view
+    }
+    
+    func elementFrame(_ element: Element) -> NSRect? {
+        guard let window = self.view.window else { return nil }
+
+        let globalFrame = GeometryUtils.convertAXFrameToGlobal(
+            element.frame)
+        let windowFrame = window.convertFromScreen(globalFrame)
+        return window.contentView?.convert(windowFrame, to: self.view)
+    }
+}
