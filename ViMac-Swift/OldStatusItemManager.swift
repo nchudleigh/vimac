bc2881e8a114e566c0509793c4b10d714ed5795e
Dexter Leng <dexterlengcs@gmail.com>
2021-03-06 16:05:00 +0800

    Create new StatusItemManager that is composed of menu and statusitems

--- ViMac-Swift/OldStatusItemManager.swift
+++ ViMac-Swift/OldStatusItemManager.swift
@@ -10,6 +10,49 @@ import Cocoa
 import Sparkle
 import Preferences
 
+class StatusItemManager: NSObject {
+    let menu: NSMenu
+    let statusItem: NSStatusItem
+    let preferencesWindowController: PreferencesWindowController
+    
+    init(preferencesWindowController: PreferencesWindowController) {
+        self.menu = NSMenu()
+        self.statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        self.statusItem.button!.image = NSImage(named: "StatusBarButtonImage")
+        self.preferencesWindowController = preferencesWindowController
+        
+        super.init()
+        
+        self.statusItem.menu = self.menu
+        self.menu.delegate = self
+    }
+}
+
+extension StatusItemManager : NSMenuDelegate {
+    func menuWillOpen(_ _menu: NSMenu) {
+        if let menu = statusItem.menu {
+            menu.removeAllItems()
+            menu.addItem(withTitle: "Preferences", action: #selector(preferencesClick), keyEquivalent: "").target = self
+            menu.addItem(withTitle: "Check for updates", action: #selector(checkForUpdatesClick), keyEquivalent: "").target = self
+            menu.addItem(NSMenuItem.separator())
+            menu.addItem(withTitle: "Quit", action: #selector(quitClick), keyEquivalent: "").target = self
+        }
+    }
+    
+    @objc func preferencesClick() {
+        preferencesWindowController.show()
+    }
+    
+    @objc func checkForUpdatesClick() {
+        SUUpdater.shared()?.checkForUpdates(nil)
+    }
+    
+    @objc func quitClick() {
+        NSApplication.shared.terminate(self)
+    }
+}
+
+
 class OldStatusItemManager: NSMenu, NSMenuDelegate, NSWindowDelegate {
     let statusItem: NSStatusItem
     let preferencesWindowController: PreferencesWindowController
