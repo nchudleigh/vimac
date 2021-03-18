8e48fd79658a0c2c1f494d7b4a8f636a477866ea
Dexter Leng <34204380+dexterleng@users.noreply.github.com>
2021-03-18 12:09:08 +0800

    Fix scrolling not working in some cases (e.g. Terminal) (#345)

--- ViMac-Swift/ChunkyScroller.swift
+++ ViMac-Swift/ChunkyScroller.swift
@@ -10,18 +10,18 @@ import Cocoa
 
 class ChunkyScroller: Scroller {
     static func instantiate(direction: ScrollDirection, scrollAmount: Int) -> ChunkyScroller {
-        var xAxis = Int64(0)
-        var yAxis = Int64(0)
+        var xAxis = Int32(0)
+        var yAxis = Int32(0)
         
         switch direction {
             case .halfLeft:
-                xAxis = Int64(scrollAmount)
+                xAxis = Int32(scrollAmount)
             case .halfRight:
-                xAxis = Int64(-scrollAmount)
+                xAxis = Int32(-scrollAmount)
             case .halfDown:
-                yAxis = Int64(-scrollAmount)
+                yAxis = Int32(-scrollAmount)
             case .halfUp:
-                yAxis = Int64(scrollAmount)
+                yAxis = Int32(scrollAmount)
             default:
                 fatalError("<direction> scroll directions should not used for SmoothScroller")
         }
@@ -45,18 +45,18 @@ class ChunkyScroller: Scroller {
     static func instantiateForSmoothScroll(direction: ScrollDirection) -> ChunkyScroller {
         let sensitivity = UserPreferences.ScrollMode.ScrollSensitivityProperty.read()
         
-        var xAxis = Int64(0)
-        var yAxis = Int64(0)
+        var xAxis = Int32(0)
+        var yAxis = Int32(0)
         
         switch direction {
             case .left:
-                xAxis = Int64(sensitivity)
+                xAxis = Int32(sensitivity)
             case .right:
-                xAxis = Int64(-sensitivity)
+                xAxis = Int32(-sensitivity)
             case .down:
-                yAxis = Int64(-sensitivity)
+                yAxis = Int32(-sensitivity)
             case .up:
-                yAxis = Int64(sensitivity)
+                yAxis = Int32(sensitivity)
             default:
                 fatalError("half-<direction> scroll directions should not used for smooth scrolling")
         }
@@ -78,21 +78,19 @@ class ChunkyScroller: Scroller {
     }
     
     private let frequency: TimeInterval
-    private let xAxis: Int64
-    private let yAxis: Int64
+    private let xAxis: Int32
+    private let yAxis: Int32
     
     private var timer: Timer?
     
-    init(frequency: TimeInterval, xAxis: Int64, yAxis: Int64) {
+    init(frequency: TimeInterval, xAxis: Int32, yAxis: Int32) {
         self.frequency = frequency
         self.xAxis = xAxis
         self.yAxis = yAxis
     }
     
     @objc func emitScrollEvent() {
-        let event = CGEvent.init(scrollWheelEvent2Source: nil, units: .pixel, wheelCount: 1, wheel1: 0, wheel2: 0, wheel3: 0)!
-        event.setIntegerValueField(.scrollWheelEventPointDeltaAxis1, value: yAxis)
-        event.setIntegerValueField(.scrollWheelEventPointDeltaAxis2, value: xAxis)
+        let event = CGEvent.init(scrollWheelEvent2Source: nil, units: .pixel, wheelCount: 2, wheel1: yAxis, wheel2: xAxis, wheel3: 0)!
         event.post(tap: .cghidEventTap)
     }
     
