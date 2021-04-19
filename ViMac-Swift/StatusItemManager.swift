6576dba99556906b0d1280c5fef4134975fa17bf
Dexter Leng <34204380+dexterleng@users.noreply.github.com>
2021-04-19 19:52:41 +0800

    Onboarding stuff (#409)
    * Move full instructions into manual.md
    
    * Add getting started and manual to menu bar item

--- ViMac-Swift/StatusItemManager.swift
+++ ViMac-Swift/StatusItemManager.swift
@@ -33,6 +33,7 @@ extension StatusItemManager : NSMenuDelegate {
         if let menu = statusItem.menu {
             menu.removeAllItems()
             menu.addItem(withTitle: "Getting Started", action: #selector(gettingStartedClick), keyEquivalent: "").target = self
+            menu.addItem(withTitle: "Manual", action: #selector(manualClick), keyEquivalent: "").target = self
             menu.addItem(withTitle: "Report bugs / Suggest features", action: #selector(openGithubIssues), keyEquivalent: "").target = self
             menu.addItem(withTitle: "Follow Vimac on Twitter", action: #selector(followVimacClick), keyEquivalent: "").target = self
             menu.addItem(NSMenuItem.separator())
@@ -54,9 +55,15 @@ extension StatusItemManager : NSMenuDelegate {
     }
     
     @objc func gettingStartedClick() {
-        let url = URL(string: "https://github.com/dexterleng/vimac#getting-started")!
+        let url = URL(string: "https://vimacapp.com/tutorial")!
         _ = NSWorkspace.shared.open(url)
     }
+    
+    @objc func manualClick() {
+        let url = URL(string: "https://vimacapp.com/manual")!
+        _ = NSWorkspace.shared.open(url)
+    }
+
     
     @objc func followVimacClick() {
         let url = URL(string: "https://twitter.com/vimacapp")!
